24673c4e24963d792ba627c0c03868dc20457ebb
Karin Baldisseri <karinbaldi@hotmail.com>
2022-09-24 09:00:07 +0200

    Add set params for id in URL

--- front/js/script.js
+++ front/js/script.js
@@ -1,38 +1,58 @@
+/*******************************************************
+ * Déclaration et initialisation des VARIABLES globales 
+ *******************************************************/
+
 const itemsSection = document.getElementById("items");
+const apiUrl = "http://localhost:3000/api/products";
+let productId;
+
+/****************************
+ * Déclaration des FONCTIONS
+ ****************************/
 
 // Récupérer les données des produits depuis l'API
 const getProducts = async () => {
     try {
-        const response = await fetch('http://localhost:3000/api/products')
+        const response = await fetch(apiUrl)
         if (response.ok) {
             return await response.json()
         } else {
             console.error('Retour du serveur : erreur', response.status)
         }
-    } catch(e) {
+    } catch (e) {
         alert(e)
     }
-}
+};
 
+// Rajouter Id du produit (paramètre) à URL
+function setParamId(product) {
+    const params = new URLSearchParams(window.location.search);
+    params.set('id', `${product._id}`);
+    productId = params.toString();
+};
+    
 // Création de la carte produit
-function showProduct(product){
+function createProduct(product) {
+    setParamId(product);
     itemsSection.innerHTML += 
-    `<a href="product.html?id=${product._id}"> 
+    `<a href="./product.html?${productId}"> 
         <article>
             <img src="${product.imageUrl}" alt="${product.altTxt}">
             <h3 class="productName">${product.name}</h3>
             <p class="productDescription">${product.description}</p>
         </article>
     </a>`
-}
+};
 
 // Affichage de tous les produits
 async function displayProducts() {
     const products = await getProducts();
     for (let product of products) {
-        showProduct(product)
+        createProduct(product)
     }
-}
+};
+
+/********************************************************************************/
 
 displayProducts();
   
